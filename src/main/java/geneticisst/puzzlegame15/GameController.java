4f28bef02e7310a2b56ee736de0762bd9b26ebab
genetyc <gigagrigiga@gmail.com>
2024-03-09 16:07:27 +0300

    Play page markup

--- src/main/java/geneticisst/puzzlegame15/GameController.java
+++ src/main/java/geneticisst/puzzlegame15/GameController.java
@@ -1,19 +1,58 @@
 package geneticisst.puzzlegame15;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Point2D;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
 
 public class GameController {
 
     public Label titleText;
     public Button menuBtn;
+    public GridPane field;
+    public Pane pane;
+    private List<Node> buttonList;
+
+    @FXML
+    private void initialize() {
+        buttonList = field.getChildren().stream().filter(it -> it instanceof Button).toList();
+        System.out.println(buttonList.size());
+    }
+
+    @FXML
+    private void onMouseMoved(MouseEvent event) {
+        double mouseX = event.getSceneX();
+        double mouseY = event.getSceneY();
+        //bot-right = 530, 630
+        //top-right = 530, 370
+        //top-left = 270, 370
+        //bot-left = 270, 630
+        //square = 65
+
+        Point2D mousePoint = pane.sceneToLocal(mouseX, mouseY);
+
+        if (field.getBoundsInParent().contains(mousePoint)) {
+            System.out.println("Курсор в пределах GridPane");
+            System.out.printf("%f -> %f", mouseX, mouseY);
+        } else {
+            System.out.println("Курсор вне GridPane");
+        }
+    }
 
     public void menuBtnPressed(ActionEvent event) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
